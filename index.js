20074afd4bd1331efaf5457203a6daa803c3febc
pinosol <zonest_64@mail.ru>
2021-11-08 13:41:45 +0300

    added basic models and routes

--- index.js
+++ index.js
@@ -3,17 +3,25 @@ import mongoose from 'mongoose';
 import cors from 'cors';
 import cookieParser from 'cookie-parser'
 import dotenv from 'dotenv'
+import router from './router/index.js'
 dotenv.config()
 
 const app = express();
 
-const PORT = process.env.PORT || 8080
+const PORT = process.env.PORT || 8080;
 
 app.use(cors());
-app.use(express.json())
+app.use(express.json());
+app.use(cookieParser());
+app.use('/api', router)
+
 
 async function startApp() {
   try {
+    await mongoose.connect(process.env.DB_URL, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true
+    })
     app.listen(PORT, () => {
       console.log(`server started at http://localhost:${PORT}`)
     })
